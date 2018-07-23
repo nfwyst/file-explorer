2ea1b3fb77fb36e363c64c3f40c43d961f61c06a
nfwyst <nfwyst@gmail.com>
2018-07-23 17:30:59 +0800

    add test spec for dir service

---
+++ js/service/Dir.spec.js
@@ -0,0 +1,36 @@
+const DirService = require("./Dir"),
+      mock = require("mock-fs"), 
+      { join } = require("path");
+
+describe("service/Dir", () => {
+  beforeEach(() => {
+    mock({
+      foo: {
+        bar: {
+          baz: "baz",
+          qux: "qux"
+        }
+      }
+    });
+  });
+  afterEach(mock.restore);
+  // 测试获取文件列表
+  describe("#getFileList", () => {
+    it("获取期望的文件列表", () => {
+      const service = new DirService(join("foo", "bar"));
+      service.setDir("bar");
+      let files = service.getFileList();
+      expect(files.length).toBe(2);
+    });
+    it("获取期望的文件属性", () => {
+      const service = new DirService(join("foo", "bar"));
+      const files = service.getFileList();
+      const [file] = files;
+      expect(file.fileName).toBe("baz");
+      expect(file.stats.size).toBe(3);
+      expect(file.stats.isFile()).toBe(true);
+      expect(file.stats.isDirectory()).toBe(false);
+      expect(file.stats.mtime).toBeTruthy();
+    });
+  });
+});
